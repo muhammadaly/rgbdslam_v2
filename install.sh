d5913d330625e96a04d940f614eda92c8bff135d
muhammadaly <muhammadaly92@gmail.com>
2017-09-24 20:22:39 +0200

    add changes on install.sh

--- install.sh
+++ install.sh
@@ -7,24 +7,24 @@
 #two occurences of "-j2" below to parallelize the compilation more
 
 #Prepare System
-if test ! -d /opt/ros/kinetic; then
-  echo This script assumes ROS kinetic to be installed
-  echo The directory /opt/ros/kinetic was not found
-  exit 1
-fi
+#if test ! -d /opt/ros/kinetic; then
+#  echo This script assumes ROS kinetic to be installed
+#  echo The directory /opt/ros/kinetic was not found
+#  exit 1
+#fi
 
-SUBDIR=~/Code
+SUBDIR=~/ros_workspaces/master
 echo "This script puts all code into '$SUBDIR'. Edit this script to change the location."
 echo "Press enter to continue, Ctrl-C to cancel"
 read
 mkdir -p $SUBDIR
 
-source /opt/ros/kinetic/setup.bash
+source /opt/ros/indigo/setup.bash
 
 echo
 echo "Removing packages known to conflict (password required for apt-get)"
 echo
-sudo apt-get purge ros-kinetic-libg2o libqglviewer-dev
+#sudo apt-get purge ros-kinetic-libg2o libqglviewer-dev
 
 echo
 echo "Updating ROS dependency database"
@@ -39,7 +39,7 @@ echo
 echo "Downloading, building and installing g2o"
 echo
 G2O_REPO_DIR=$SUBDIR/g2ofork
-git clone -b c++03 https://github.com/felixendres/g2o.git $G2O_REPO_DIR
+git clone https://github.com/felixendres/g2o.git $G2O_REPO_DIR
 mkdir $G2O_REPO_DIR/build
 cd $G2O_REPO_DIR/build
 cmake .. -DCMAKE_INSTALL_PREFIX=$G2O_REPO_DIR/install -DG2O_BUILD_EXAMPLES=OFF
@@ -60,7 +60,7 @@ echo "Downloading rgbdslam_v2"
 echo
 #Get and build rgbdslam_v2
 export G2O_DIR=$G2O_REPO_DIR/install
-git clone -b kinetic https://github.com/felixendres/rgbdslam_v2.git $WORKSPACE/src/rgbdslam
+git clone -b kinetic https://github.com/muhammadaly/rgbdslam_v2 $WORKSPACE/src/rgbdslam
 
 #Install missing dependencies
 rosdep install rgbdslam
