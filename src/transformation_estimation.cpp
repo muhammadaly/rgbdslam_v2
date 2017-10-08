c96c5840d681b95bce2f1d8bef88549866b1ccfa
muhammadaly <muhammadaly92@gmail.com>
2017-10-08 23:35:42 +0200

    add code

--- src/transformation_estimation.cpp
+++ src/transformation_estimation.cpp
@@ -168,3 +168,45 @@ void getTransformFromMatchesG2O(const Node* earlier_node,
   transformation_estimate = cams.first->estimate().cast<float>().inverse().matrix();
   delete optimizer;
 }
+
+void getTransformFromMatchesPCL(const Node* earlier_node,
+                                const Node* newer_node,
+                                const std::vector<cv::DMatch> & matches,
+                                Eigen::Matrix4f& transformation_estimate, //Input (initial guess) and Output
+                                int iterations)
+{
+  ScopedTimer s(__FUNCTION__);
+
+  const float leaf = 0.005f;
+  int MaximumIterations = 50000; // Number of RANSAC iterations
+  int NumberOfSamples = 3; // Number of points to sample for generating/prerejecting a pose
+  int CorrespondenceRandomness = 5; // Number of nearest features to use
+  float SimilarityThreshold = 0.9f; // Polygonal edge length similarity threshold
+  float MaxCorrespondenceDistance = 2.5f* leaf; // Inlier threshold 0.015
+  float InlierFraction = 0.25f; // Required inlier fraction for accepting a pose hypothesis
+
+  pcl::SampleConsensusPrerejective<PointT,PointT,FeatureT> align;
+  align.setInputSource(secondPC);
+  align.setSourceFeatures(secondPCFeatures);
+  align.setInputTarget(firstPC);
+  align.setTargetFeatures(firstPCFeatures);
+
+  align.setMaximumIterations(MaximumIterations); // Number of RANSAC iterations
+  align.setNumberOfSamples(NumberOfSamples); // Number of points to sample for generating/prerejecting a pose
+  align.setCorrespondenceRandomness(CorrespondenceRandomness); // Number of nearest features to use
+  align.setSimilarityThreshold(SimilarityThreshold); // Polygonal edge length similarity threshold
+  align.setMaxCorrespondenceDistance(MaxCorrespondenceDistance); // Inlier threshold
+  align.setInlierFraction(InlierFraction); // Required inlier fraction for accepting a pose hypothesis
+  align.align(*alignedPC);
+
+  if(align.hasConverged())
+  {
+    transformation_estimate = align.getFinalTransformation();
+    // return true;
+  }
+  else
+  {
+//    pcl::console::print_error("Alignment failed!\n");
+    // return false;
+  }
+}
